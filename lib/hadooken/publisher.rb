9fb08fbbae87f5ea93aaab761b6de71804f118e2
Mehmet Emin INAC <mehmetemininac@gmail.com>
2019-07-17 18:48:55 +0200

    Add options parameter to interface of publishers

--- lib/hadooken/publisher.rb
+++ lib/hadooken/publisher.rb
@@ -22,16 +22,16 @@ module Hadooken
         @topic || raise(Errors::MissingTopic.new(self))
       end
 
-      def meta
-        { version: version, name: message_name, time: time }
+      def meta(computed_message_name)
+        { version: version, name: computed_message_name, time: time }
       end
 
-      def publish(object)
-        new(object).publish
+      def publish(object, **options)
+        new(object, options).publish
       end
 
-      def produce(payload)
-        Hadooken.producer.produce(payload, topic: topic)
+      def produce(payload, computed_topic)
+        Hadooken.producer.produce(payload, topic: computed_topic)
       end
 
       private
@@ -53,28 +53,37 @@ module Hadooken
 
     end
 
-    attr_reader :object
+    attr_reader :object, :options
 
     delegate :meta, :serializer, to: :"self.class"
 
-    def initialize(object)
+    def initialize(object, **options)
       @object = object
+      @options = options
     end
 
     def publish
-      self.class.produce(payload)
+      self.class.produce(payload, topic)
     end
 
     private
       def payload
         {
-          meta: meta,
+          meta: meta(message_name),
           data: data
         }.to_json
       end
 
       def data
         serializer.new(object).as_json
+      end
+
+      def topic
+        self.class.topic.respond_to?(:call) ? instance_exec(&self.class.topic) : self.class.topic
+      end
+
+      def message_name
+        self.class.message_name.respond_to?(:call) ? instance_exec(&self.class.message_name) : self.class.message_name
       end
 
   end
